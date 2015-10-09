fdc11b3272217bcd9079847968d68366e51e75e8
tedler <tedler@gmail.com>
2015-10-10 10:48:02 +1100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt) {
+
+  //Initializing the configuration object
+  
+    grunt.initConfig({
+
+      // Task configuration
+    
+    less: {
+        development: {
+            options: {
+              compress: true,  //minifying the result
+            },
+            files: {
+              //compiling style.less into style.css
+              "public/css/style.css":"assets/less/style.less"
+            }
+        }
+    },
+
+    imagemin: {
+        dynamic: {                         
+          files: [{
+            expand: true,                  
+            cwd: 'assets/images/',                   
+            src: ['**/*.{png,jpg,gif,svg}'],
+            dest: 'public/images/'                 
+          }]
+        }
+      },
+    
+    jshint: {
+        dev: {        
+            src: ['assets/js/script.js']
+        }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      js_frontend: {
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/bootstrap/dist/js/bootstrap.js',
+          'assets/js/script.js'
+        ],
+        dest: 'public/js/script.js',
+      },
+    },
+    
+    uglify: {
+      options: {
+        mangle: false  // Use if you want the names of your functions and variables unchanged
+      },
+      frontend: {
+        files: {
+          'public/js/script.js': 'public/js/script.js',
+        }
+      },
+    },
+    
+    watch: {
+        js_frontend: {
+          files: ['assets/js/script.js'],   
+          tasks: ['jshint','concat:js_frontend','uglify:frontend'],     //tasks to run
+          options: {
+            livereload: true                        //reloads the browser
+          }
+        },
+
+        imagemin: {
+          files: ['assets/images/**/*.{png,jpg,gif,svg}'],
+          tasks: ['imagemin']
+        },
+        
+        less: {
+          files: ['assets/less/*.less'],  //watched files
+          tasks: ['less'],                          //tasks to run
+          options: {
+            livereload: true                        //reloads the browser
+          }
+        },
+
+       livereload: {
+          files: ['public/**/*.*'],
+          options: { livereload: true }
+        },
+
+        
+      }
+    });
+
+  // Plugin loading
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-livereload');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Task definition
+  grunt.registerTask('default', ['watch','imagemin']);
+
+};
---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+// Top section "Background Height & Content Top Margin" resizer
+
+	$(window).resize(function() {
+
+		var WinHeight = $(window).height();
+	    $('#featured').height(WinHeight - 150);
+	    $('.content').css('top', WinHeight - 100);
+
+	});
+
+	$(window).trigger('resize');
+
+
+// Comments for the next section
+	
+
+});
