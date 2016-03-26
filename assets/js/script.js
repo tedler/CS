608852b9f347ec484e28f2d853464481f14a4972
tedler <tedler@gmail.com>
2016-03-26 22:04:57 +1100

    New update

--- assets/js/script.js
+++ assets/js/script.js
@@ -431,7 +431,35 @@ $(document).ready(function() {
 	});
 
 
+// Waypoints for OnScroll animate.css
 
+	function onScrollInit( items, trigger ) {
+	  items.each( function() {
+
+	  	var osOffset = ($(window).height() * 0.75) + 300;
+	    var osElement = $(this),
+	        osAnimationClass = osElement.attr('data-os-animation'),
+	        osAnimationDelay = osElement.attr('data-os-animation-delay');
+	      
+	        osElement.css({
+	          '-webkit-animation-delay':  osAnimationDelay,
+	          '-moz-animation-delay':     osAnimationDelay,
+	          'animation-delay':          osAnimationDelay
+	        });
+
+	        var osTrigger = ( trigger ) ? trigger : osElement;
+	        
+	        osTrigger.waypoint(function() {
+	          osElement.addClass('animated').addClass(osAnimationClass);
+	          },{
+	              triggerOnce: true,
+	              offset: osOffset
+	        });
+	  });
+	}
+
+	 onScrollInit( $('.os-animation') );
+	 onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
 
 
 });
